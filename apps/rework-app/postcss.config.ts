d8bfc3c35be413555d13feee43a1bd628b0009bf
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-09-11 12:58:44 +0600

    chore(rework-app): add internal @workspace/shadcn-ui package.

--- apps/rework-app/postcss.config.ts
+++ apps/rework-app/postcss.config.ts
@@ -1,5 +1,2 @@
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
+// biome-ignore lint/performance/noBarrelFile: <"This is the standard procedure">
+export { default } from "@workspace/shadcn-ui/postcss.config";
